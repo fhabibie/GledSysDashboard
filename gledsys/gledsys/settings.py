668f833bd28cfbfddae6d7c66e5fcbc696a739c2
fhabibie <fhabibie14@gmail.com>
2022-09-05 16:38:17 +0700

    fix postgres db healthcheck interval

--- gledsys/gledsys/settings.py
+++ gledsys/gledsys/settings.py
@@ -75,14 +75,6 @@ WSGI_APPLICATION = "gledsys.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.environ.get('POSTGRES_NAME'),
-    #     'USER': os.environ.get('POSTGRES_USER'),
-    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-    #     'HOST': 'db',
-    #     'PORT': 5432,
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME'),
