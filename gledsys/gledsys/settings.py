9d244831250bfecb215a22e59e9d51209c310681
fhabibie <fhabibie14@gmail.com>
2022-09-08 18:47:50 +0700

    add media path

--- gledsys/gledsys/settings.py
+++ gledsys/gledsys/settings.py
@@ -121,6 +121,7 @@ MEDIA_URL = "media/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
