19b6aadf0e36a30a9eb3d1a74341581797e26270
fhabibie <fhabibie14@gmail.com>
2022-09-16 23:24:50 +0700

    add plotly

--- gledsys/maps/views.py
+++ gledsys/maps/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-from modules.chart import get_lighting_bar_chart, get_lighting_pie_chart
+from modules.chart import get_lightning_bar_chart, get_lightning_pie_chart, get_lightning_pie_chart_plotly, get_lightning_bar_chart_plotly
 from uploads.models import Lightning
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -18,7 +18,7 @@ def lighning_distribution_maps(request):
 
 def lighning_temporal_chart(request):
 
-    # chart = get_lighting_bar_chart()
+    # chart = get_lightning_bar_chart()
     # Get initial chart
     query = Lightning.objects.filter(
             datetime_utc__range = ["2020-09-16T19:16:54+07:00", "2021-09-16T19:16:54+07:00"],
@@ -28,19 +28,26 @@ def lighning_temporal_chart(request):
 
     print('query len', len(query))
     if len(query) > 0:
-        bar_chart = get_lighting_bar_chart(query)
+        bar_chart = get_lightning_bar_chart(query)
         bar_chart = "data:image/png;base64, " + bar_chart
-        pie_chart = get_lighting_pie_chart(query)
+        pie_chart = get_lightning_pie_chart(query)
         pie_chart = "data:image/png;base64, " + pie_chart
+        pie_plotly = get_lightning_pie_chart_plotly(query)
+        bar_plotly = get_lightning_bar_chart_plotly(query)
+
     else:
         # Add default img
         bar_chart = ''
         pie_chart = ''
+        pie_plotly = ''
+        bar_plotly = ''
 
     context = {
         "page_title": "Lightning Temporal Statistics",
         "bar_chart": bar_chart,
-        "pie_chart": pie_chart
+        "pie_chart": pie_chart,
+        "pie_plotly": pie_plotly,
+        "bar_plotly": bar_plotly,
     }
     return render(request, 'lightning_temporal.html', context)
 
@@ -59,11 +66,15 @@ def get_lightning_temporal_chart(request):
             ).values_list("datetime_utc", "type")
 
         if len(query) > 0:
-            bar_chart = get_lighting_bar_chart(query)
+            bar_chart = get_lightning_bar_chart(query)
             bar_chart = "data:image/png;base64, " + bar_chart
-            pie_chart = get_lighting_pie_chart(query)
+            pie_chart = get_lightning_pie_chart(query)
             pie_chart = "data:image/png;base64, " + pie_chart
-            return JsonResponse({"success":True, "data": {"bar": bar_chart, "pie": pie_chart}}, status = 200)
+
+            pie_plotly = get_lightning_pie_chart_plotly(query)
+            bar_plotly = get_lightning_bar_chart_plotly(query)
+
+            return JsonResponse({"success":True, "data": {"bar": bar_chart, "pie": pie_chart, "pieplotly": pie_plotly, "barplotly": bar_plotly}}, status = 200)
         else:
             return JsonResponse({"success": True,}, status = 200)
 
--- gledsys/modules/chart.py
+++ gledsys/modules/chart.py
@@ -1,6 +1,7 @@
 import base64
 from io import BytesIO
 import matplotlib.pyplot as plt
+import plotly.express as px
 import numpy as np
 import pandas as pd
 
@@ -16,7 +17,7 @@ def get_base64():
 
     return res
 
-def get_lighting_bar_chart(obj):
+def get_lightning_bar_chart(obj):
     plt.switch_backend('AGG')
     df = pd.DataFrame(list(obj), columns=["datetime_utc", "type"])
     df['datetime_utc'] = df['datetime_utc'].dt.strftime('%m/%d/%Y')
@@ -28,13 +29,29 @@ def get_lighting_bar_chart(obj):
     chart = get_base64()
     return chart
 
-def get_lighting_pie_chart(obj):
+def get_lightning_pie_chart(obj):
     plt.switch_backend('AGG')
     df = pd.DataFrame(list(obj), columns=["datetime_utc", "type"])
-    df['datetime_utc'] = df['datetime_utc'].dt.strftime('%m/%d/%Y')
     df['category'] = df['type'].apply(lambda x: 'IC' if x == 2 else 'GC-' if x == 1 else 'GC+')
     df = df.groupby(['category']).size()
     df.plot(kind='pie', y='category', figsize=(8,5), autopct='%1.0f%%', title="Pie Lightning Statistics")
 
     chart = get_base64()
     return chart
+
+def get_lightning_pie_chart_plotly(obj):
+    df = pd.DataFrame(list(obj), columns=["datetime_utc", "type"])
+    df['category'] = df['type'].apply(lambda x: 'IC' if x == 2 else 'GC-' if x == 1 else 'GC+')
+    df = df.groupby(['category'], as_index=False, group_keys=True).size()
+    fig = px.pie(df, values='size', names='category')
+
+    return fig.to_html()
+
+def get_lightning_bar_chart_plotly(obj):
+    df = pd.DataFrame(list(obj), columns=["datetime_utc", "type"])
+    df['datetime_utc'] = df['datetime_utc'].dt.strftime('%m/%d/%Y')
+    df['category'] = df['type'].apply(lambda x: 'IC' if x == 2 else 'GC-' if x == 1 else 'GC+')
+    df = df.groupby(['datetime_utc', 'category'], as_index=False).size()
+    fig = px.bar(df,x="datetime_utc", y="size", color="category", title="Bar chart")
+
+    return fig.to_html()
