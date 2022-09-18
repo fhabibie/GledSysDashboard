3e232e7dcfe7fd5b871ddb0dbc132dee35253240
fhabibie <fhabibie14@gmail.com>
2022-09-18 18:47:38 +0700

    add pygmt map example

--- gledsys/maps/views.py
+++ gledsys/maps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from modules.chart import get_lightning_bar_chart, get_lightning_pie_chart, get_lightning_pie_chart_plotly, get_lightning_bar_chart_plotly
+from modules.map import get_distribution_map
 from uploads.models import Lightning
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -9,24 +10,39 @@ from django.utils.dateparse import parse_datetime
 # Create your views here.
 
 def lighning_distribution_maps(request):
+    query = Lightning.objects.filter(
+            datetime_utc__range = ["2021-02-01T19:16:54+07:00", "2021-02-02T19:16:54+07:00"],
+            latitude__range = [-6.34232915063942, 1.3138862223887],
+            longitude__range = [94.0034031691933, 95.2507765200479],
+            type__in = [0,1,2]).values_list("datetime_utc", "latitude", "longitude", "type")
+    print(len(query))
+    
+    dist_map = get_distribution_map(query)
     context = {
         "page_title": "Lightning Distribution Maps",
+        "map": "data:image/png;base64, " + dist_map
     }
     
     return render(request, 'lightning_distribution.html', context)
 
 
 def lighning_temporal_chart(request):
+    # Initial Dataset
+    # from django.contrib.gis.geos import Polygon
 
-    # chart = get_lightning_bar_chart()
-    # Get initial chart
+    # bbox = (-6.9145,53.5958,-5.6085,53.1023)#min_lat,max_lat,min_lng,max_lng 
+    # geom = Polygon.from_bbox(bbox)
+
+    # Location.objects.filter(point__within=geom)
+    
+    
     query = Lightning.objects.filter(
             datetime_utc__range = ["2020-09-16T19:16:54+07:00", "2021-09-16T19:16:54+07:00"],
             latitude__range = [-6.34232915063942, 1.3138862223887],
             longitude__range = [94.0034031691933, 95.2507765200479],
             type__in = [0,1,2]).values_list("datetime_utc", "type")
+    bar_chart, pie_chart, bar_plotly, pie_plotly = "", "", "", ""
 
-    print('query len', len(query))
     if len(query) > 0:
         bar_chart = get_lightning_bar_chart(query)
         bar_chart = "data:image/png;base64, " + bar_chart
@@ -35,12 +51,6 @@ def lighning_temporal_chart(request):
         pie_plotly = get_lightning_pie_chart_plotly(query)
         bar_plotly = get_lightning_bar_chart_plotly(query)
 
-    else:
-        # Add default img
-        bar_chart = ''
-        pie_chart = ''
-        pie_plotly = ''
-        bar_plotly = ''
 
     context = {
         "page_title": "Lightning Temporal Statistics",
---
+++ gledsys/modules/map.py
@@ -0,0 +1,35 @@
+from uploads.models import Lightning
+import geopandas as gpd
+import pandas as pd
+import pygmt
+import base64
+
+def get_distribution_map(obj):
+    # df = gpd.GeoDataFrame(list(obj), columns=["datetime_utc", "coord", "type"])
+    df = pd.DataFrame(list(obj), columns=["datetime_utc", "latitude", "longitude", "type"])
+    df_gcp = df[df.type == 0]
+    df_gcn = df[df.type == 1]
+    df_ic = df[df.type == 2]
+
+    region = [
+        df.longitude.min() - 8,
+        df.longitude.max() + 8,
+        df.latitude.min() - 8,
+        df.latitude.max() + 8,
+    ]
+
+    # region = [ 94, 144, -12, 10] # Indonesia reg
+
+    print(region)
+    fig = pygmt.Figure()
+    fig.basemap(region=region, projection="M15c", frame=True)
+    fig.coast(land="black", water="skyblue")
+    fig.plot(x=df_ic.longitude, y=df_ic.latitude, style="c0.2c", color="magenta4", pen="black")
+    fig.plot(x=df_gcp.longitude, y=df_gcp.latitude, style="khurricane/0.3c", color="seagreen", pen="black")
+    fig.plot(x=df_gcn.longitude, y=df_gcn.latitude, style="kflash/0.3c", color="darkorange", pen="black")
+
+    raw_png = fig._preview(fmt="png", dpi=300, anti_alias=True, as_bytes=True)
+    base64_png = base64.encodebytes(raw_png)
+    res = base64_png.decode("utf-8")
+
+    return res
